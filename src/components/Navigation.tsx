06592781b14b0dd11eaab53c020be66c275bb7bd
Prado <jv_prado@outlook.com>
2025-05-30 16:31:57 -0300

    fix

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -35,7 +35,7 @@ const Navigation = () => {
             className="text-2xl font-bold text-white"
           >
             <img
-              src="/prado-lettering.svg"
+              src="/portfoliodev/prado-lettering.svg"
               alt="Logo João Victor Prado"
               className="h-35 w-auto text-white brightness-0 invert"
             />
